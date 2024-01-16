d32a7351dc26f7e5b7bbbdc74723f563042c5c72
HsuXinYu <jolin7180732@gmail.com>
2024-01-16 18:33:49 +0800

    vue axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  mounted() {
+    this.axios.get('https://randomuser.me/api/').then((response) => {
+      // console.log(response.data)
+      this.user = response.data.results[0]
+      console.log(this.user)
+    })
+    // or
+    // this.$http.get(api).then((response) => {
+    //   console.log(response.data)
+    // })
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ this.user.name.first }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
