76f5f490433f3be761560084a84a8e8f7dcf93c9
xinyu <jolin7180732@gmail.com>
2024-02-22 17:40:56 +0800

    vue pinia

---
+++ src/components/CartComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bg-light p-4 my-4">
+    <div v-if="!cartList.carts.length">購物車沒有任何品項</div>
+    <table class="table align-middle" v-else>
+      <tbody v-for="item in cartList.carts" :key="item.id">
+        <tr>
+          <td width="100">
+            <a href="#" class="text-dark" @click.prevent="removeFromCart(item.id)"> × </a>
+          </td>
+          <td width="100">
+            <img :src="item.product.imageUrl" class="table-image" alt="" />
+          </td>
+          <td>{{ item.product.title }}</td>
+          <td width="200">
+            <select
+              name=""
+              id=""
+              class="form-select"
+              :value="item.quantity"
+              @change="(event) => setCartQuantity(item.id, event)"
+            >
+              <option :value="i" v-for="i in 10" :key="i">{{ i }}</option>
+            </select>
+          </td>
+          <td width="200" class="text-end">$ {{ item.product.price }}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="5" class="text-end">總金額 NT$ {{ cartList.total }}</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import cartStore from '../stores/cartStore.js'
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(cartStore, ['cartList'])
+  },
+  methods: {
+    ...mapActions(cartStore, ['removeFromCart', 'setCartQuantity'])
+  }
+}
+</script>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar bg-light">
+    <div class="container-fluid">
+      <span class="navbar-brand" href="#">甜心甜點店</span>
+      <div>
+        購物車
+        <span class="badge rounded-pill bg-danger text-white">{{ cart.length }}</span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import cartStore from '../stores/cartStore.js'
+import { mapState } from 'pinia'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(cartStore, ['cart'])
+  }
+}
+</script>
